bb3ed4bd2434c0015bad137ebc07e339325d18f0
mathcovax <98911237+mathcovax@users.noreply.github.com>
2024-09-25 15:29:47 +0000

    feat(38): integration test preflight

---
+++ test/spec/preflight/withSkipOnRoute.ts
@@ -0,0 +1,56 @@
+import { OkHttpResponse, useBuilder, zod } from "@scripts/index";
+import type { ExpectType } from "@test/utils/expectType";
+import { fixtureProcessWichDropValue } from "@test/utils/fixture";
+
+const extractProcess = useBuilder()
+	.createProcess("extractProcess")
+	.extract({ body: zod.number() })
+	.exportation(["body"]);
+
+export const preflightWithSkipOnRoute = useBuilder()
+	.preflight(
+		extractProcess,
+		{ pickup: ["body"] },
+	)
+	.preflight(
+		fixtureProcessWichDropValue,
+		{
+			pickup: ["dropInput", "dropOptions"],
+			skip: (pickup) => {
+				const { body } = pickup(["body"]);
+
+				type check1 = ExpectType<
+					typeof body,
+					number,
+					"strict"
+				>;
+
+				return !!body;
+			},
+		},
+	)
+	.createRoute("GET", "/")
+	.handler(
+		(pickup) => {
+			const { dropInput, dropOptions } = pickup(["dropInput", "dropOptions"]);
+			type check1 = ExpectType<
+				typeof dropInput,
+				number | undefined,
+				"strict"
+			>;
+
+			type check2 = ExpectType<
+				typeof dropOptions,
+				{
+					option1: string;
+					option2: number;
+				} | undefined,
+				"strict"
+			>;
+
+			return new OkHttpResponse(undefined, {
+				dropInput,
+				dropOptions,
+			});
+		},
+	);

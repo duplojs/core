9855c22276b7a5c172d2e9eb01467a6fb97832c9
mathcovax <campani.mathieu@gmail.com>
2025-02-08 17:39:23 +0000

    fix(106): update peerDependencies and upgrade devDependencies

---
+++ test/setup.ts
@@ -0,0 +1,17 @@
+import { clone } from "@utils/clone";
+import { ZodError } from "zod";
+
+expect.addEqualityTesters([
+	function(expectValue, wantedValue) {
+		if (expectValue instanceof ZodError && wantedValue instanceof ZodError) {
+			const { addIssue: _addIssue1, addIssues: _addIssues1, ...expectValueWhoCanCompare } = clone(expectValue);
+			const { addIssue: _addIssue2, addIssues: _addIssues2, ...wantedValueWhoCanCompare } = clone(wantedValue);
+
+			expect(expectValueWhoCanCompare).toEqual(wantedValueWhoCanCompare);
+
+			return true;
+		} else {
+			return undefined;
+		}
+	},
+]);
